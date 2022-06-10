6721de82355717215552bf4e3d06e5fb5b8278e5
Aaryan Bhurtel <107060344+aaryan66@users.noreply.github.com>
2022-06-10 22:54:02 +0545

    Update number_guesser.py

--- number_guesser.py
+++ number_guesser.py
@@ -1,1 +1,12 @@
-print("Welcome to the number game app"); 
+import random
+print("""Welcome to the number game app
+Here we you need to choose a secret
+number which is between 1 - 10 and you
+will get only 2 chances""");
+
+#secret number
+secret_number = int(random.uniform(0,10))
+guess_number = 0
+guess_count = 2
+if guess_count>guess_number:
+    aaryan  = ()
