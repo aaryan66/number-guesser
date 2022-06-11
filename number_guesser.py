21016fb62ca6872542174d79f53aafae8fd022df
Aaryan Bhurtel <107060344+aaryan66@users.noreply.github.com>
2022-06-11 09:16:48 +0545

    Update number_guesser.py

--- number_guesser.py
+++ number_guesser.py
@@ -1,17 +1,1 @@
-import random
-print ('Welcome to the number guesser app')
-
-secretnumber = random.uniform(0,10)
-guess_count = 0
-guess_limit = 5
-
-while guess_count<guess_limit : 
-    print("Number :")
-    guess = int(input())
-    guess_count +=1
-    if guess == secretnumber : 
-        print('You won')
-        break
-    else  :
-        print('You lost try again')
-        print(f"The real answer was {secretnumber}")
+#building number guesser
