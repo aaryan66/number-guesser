0c59b624b4c1bbdf0ab561c634e7afd7c6522c3a
Aaryan Bhurtel <107060344+aaryan66@users.noreply.github.com>
2022-06-11 09:38:41 +0545

    Update number_guesser.py

--- number_guesser.py
+++ number_guesser.py
@@ -1,1 +1,18 @@
-#building number guesser
+from cmath import e
+import random
+print ('Welcome to the number guesser app')
+
+secretnumber = int(random.uniform(1,10))
+guess_count = 0
+guess_limit = 5
+
+while guess_count<guess_limit : 
+    print("Number :")
+    guess = int(input())
+    guess_count +=1
+    if guess == secretnumber : 
+        print('You won')
+        break
+else :
+ print('You lost try again')
+ print(f"The real answer was {secretnumber}")
