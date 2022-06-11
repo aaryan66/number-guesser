c4850379418f318a454ea8a8a230b81c455f68d6
Aaryan Bhurtel <107060344+aaryan66@users.noreply.github.com>
2022-06-11 07:51:37 +0545

    Update number_guesser.py

--- number_guesser.py
+++ number_guesser.py
@@ -1,12 +1,24 @@
+from cmath import e
 import random
 print("""Welcome to the number game app
 Here we you need to choose a secret
 number which is between 1 - 10 and you
-will get only 2 chances""");
+will get only 3 chances""");
 
 #secret number
 secret_number = int(random.uniform(0,10))
-guess_number = 0
-guess_count = 2
-if guess_count>guess_number:
-    aaryan  = ()
+guess_count = 0
+guess_limit = 3
+goon = "Yes"; 
+
+while guess_count<guess_limit:
+   guess = int(input("Number : "))
+   guess_count  += 1
+   if guess == secret_number:
+    print("Congratulations you won")
+    break 
+else : 
+    print ("sorry you failed")
+    print (f"The real answer was {secret_number}")
+
+
