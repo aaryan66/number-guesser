df61ae85746537d0c7a60f0909dc00be3704bf17
Aaryan Bhurtel <107060344+aaryan66@users.noreply.github.com>
2022-06-11 09:10:20 +0545

    Update number_guesser.py

--- number_guesser.py
+++ number_guesser.py
@@ -8,4 +8,10 @@ guess_limit = 5
 while guess_count<guess_limit : 
     print("Number :")
     guess = int(input())
-    
+    guess_count +=1
+    if guess == secretnumber : 
+        print('You won')
+        break
+    else  :
+        print('You lost try again')
+        print(f"The real answer was {secretnumber}")
